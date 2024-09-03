96040472eaa90be16f7c6704b572d8acb86e7a17
KenPerez <kendarylperez.yns@gmail.com>
2024-09-03 11:56:32 +0800

    Update validation rules for first_name and last_name fields in ProfileUpdateRequest and RegisterUserRequest

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,8 +17,8 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name' => ['required', 'string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
+            'first_name' => ['required', 'string', 'max:50'],
+            'last_name' => ['required', 'string', 'max:50'],
             'email' => [
                 'required',
                 'string',
